78fb23d85ca70b6995dc47f88243767010d14857
Anianonymous <hegdeanirudh2003@gmail.com>
2024-02-14 18:40:37 +0530

    Initial commit

---
+++ src/main/java/com/mentos/wheltoze/WheltozeApplication.java
@@ -0,0 +1,13 @@
+package com.mentos.wheltoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheltozeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheltozeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mentos/wheltoze/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.mentos.wheltoze.dao;
+
+import com.mentos.wheltoze.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
